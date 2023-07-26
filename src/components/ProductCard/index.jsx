f98802cfd2945ca7d934bc8d9eb192ed3292dd7d
Jessica lopez <jessicaandrealopezobando@gmail.com>
2023-07-26 10:48:04 -0500

    Button edit

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -5,6 +5,7 @@ const ProductCard = ({product}) => {
     return (
         <li className="product-card">
             {product.label} ${product.price}
+            <button className="button-edit">Editar</button>
         </li>
     )
 }
